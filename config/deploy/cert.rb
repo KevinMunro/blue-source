fee9ac023d59bc9d39c0eacecd08d9448eff73c4
Lew Gordon <LGordon2@gmail.com>
2014-09-30 14:53:31 -0400

    Cleaning up capistrano

--- config/deploy/cert.rb
+++ config/deploy/cert.rb
@@ -20,7 +20,7 @@ set :linked_files, %w(db/cert.sqlite3 config/api.yml config/initializers/bs_secr
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-set :branch, :saml
+set :branch, :staging
 set :migration_role, :web
 
 # Default deploy_to directory is /var/www/my_app
